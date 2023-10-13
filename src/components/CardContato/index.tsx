249118f9b59e8e13b14157d8139a8daf5c093e97
Guilherme <guilherme@exemplo.com>
2023-10-13 16:22:03 -0300

    adicionado os icones de salvar e cancelar edicao

--- src/components/CardContato/index.tsx
+++ src/components/CardContato/index.tsx
@@ -5,6 +5,8 @@ import RegularStar from '../../assets/star-regular.svg'
 import SolidStar from '../../assets/star-solid.svg'
 import Editar from '../../assets/pen-to-square-solid.svg'
 import Deletar from '../../assets/trash-solid.svg'
+import Salvar from '../../assets/floppy-disk-solid.svg'
+import Cancelar from '../../assets/ban-solid.svg'
 
 type Props = {
   email: string
@@ -12,23 +14,34 @@ type Props = {
 
 const CardContato = ({ email }: Props) => {
   const [favoritado, setFavoritado] = useState(false)
+  const [estaEditando, setEstaEditando] = useState(false)
 
-  // Função para alternar o estado de favoritado
   const alternarFavorito = () => {
     setFavoritado(!favoritado)
   }
 
+  const editandoCard = () => {
+    setEstaEditando(!estaEditando)
+  }
+
   return (
     <S.CardContent>
-      <S.Icons>
-        {favoritado ? (
-          <img src={SolidStar} onClick={alternarFavorito} />
-        ) : (
-          <img src={RegularStar} onClick={alternarFavorito} />
-        )}
-        <img src={Editar} />
-        <img src={Deletar} />
-      </S.Icons>
+      {estaEditando ? (
+        <S.Icons>
+          <img src={Salvar} onClick={editandoCard} />
+          <img src={Cancelar} onClick={editandoCard} />
+        </S.Icons>
+      ) : (
+        <S.Icons>
+          {favoritado ? (
+            <img src={SolidStar} onClick={alternarFavorito} />
+          ) : (
+            <img src={RegularStar} onClick={alternarFavorito} />
+          )}
+          <img src={Editar} onClick={editandoCard} />
+          <img src={Deletar} />
+        </S.Icons>
+      )}
 
       <S.FotoContato
         src="https://img.freepik.com/vetores-gratis/paisagem-gradiente-com-portao-torii-na-agua_23-2148660095.jpg"
